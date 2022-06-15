cc8436a89e845b2970ba55320f31d4daaeebdb61
Tanvir Ahmed Talukder <rahidt78@gmail.com>
2022-06-15 21:42:31 +0600

    Implement scrolling.

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 'use strict';
 
-///////////////////////////////////////
-// Modal window
-
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+const navLinks = document.querySelector('.nav__links');
 
-const openModal = function () {
+///////////////////////////////////////
+// Modal window
+
+const openModal = function (e) {
+  e.preventDefault();
+
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -26,5 +31,46 @@ overlay.addEventListener('click', closeModal);
 document.addEventListener('keydown', function (e) {
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
+  }
+});
+
+// Scrolling
+
+btnScrollTo.addEventListener('click', function () {
+  // Old way
+  /*
+  const s1Cords = section1.getBoundingClientRect();
+  window.scrollTo({
+    left: s1Cords.left + window.pageXOffset,
+    top: s1Cords.top + window.pageYOffset,
+    behavior: 'smooth',
+  });
+  */
+
+  // Modern way
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+// Page Navigation
+// Inefficient
+/*
+const navLink = document.querySelectorAll('.nav__link');
+navLink.forEach(function (el) {
+  el.addEventListener('click', function (e) {
+    e.preventDefault();
+    const id = document.querySelector(this.getAttribute('href'));
+
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  });
+});
+*/
+
+// Efficient
+navLinks.addEventListener('click', function (e) {
+  e.preventDefault();
+  // Matching strategy for ignoring click that are not nav link
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
   }
 });
